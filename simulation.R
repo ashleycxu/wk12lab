6de70da1887cd663a92ee88779c6878cb3d6231c
hienbui1001 <hien_bui@brown.edu>
2025-11-19 14:13:49 -0500

    Update simulation.R

--- simulation.R
+++ simulation.R
@@ -1,5 +1,8 @@
 # Function for simulating data
 
-simulate_day <- function(station_a, station_b, arrival_rates){
-  
+simulate_day <- function(arrival_rates){
+  sim_data <- arrival_rates %>%
+    group_by(start_station, end_station) %>%
+    summarise(lamda_max = max(mu_hat))
 }
+
